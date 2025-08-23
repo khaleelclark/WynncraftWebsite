f6d27bfe53225a7cc77b9b50321e2ec53a59bfc5
Thoposaurus <149897944+khaleelclark@users.noreply.github.com>
2025-08-23 00:19:03 -0400

    Configured postgres and set up enviornment variable support

--- src/main/java/com/example/playercrud/PlayerCrudApplication.java
+++ src/main/java/com/example/playercrud/PlayerCrudApplication.java
@@ -1,5 +1,6 @@
 package com.example.playercrud;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PlayerCrudApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
         SpringApplication.run(PlayerCrudApplication.class, args);
     }
 }
