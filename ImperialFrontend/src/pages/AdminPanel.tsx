82925e228b0bd92cfddc1f1cabd371f078b2fef4
Thoposaurus <149897944+khaleelclark@users.noreply.github.com>
2025-12-21 20:20:05 -0500

    Removed react import statements

--- ImperialFrontend/src/pages/AdminPanel.tsx
+++ ImperialFrontend/src/pages/AdminPanel.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
 
@@ -7,7 +6,13 @@ const AdminPanel: React.FC = () => {
   return (
     <div style={{ padding: 32 }}>
       <h2>Admin Panel</h2>
-      <Button variant="contained" color="primary" onClick={() => navigate("/events")}>Go to Event Table</Button>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => navigate("/events")}
+      >
+        Go to Event Table
+      </Button>
       {/* More admin features will go here */}
     </div>
   );
