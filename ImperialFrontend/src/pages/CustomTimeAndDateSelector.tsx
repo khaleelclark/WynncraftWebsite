8becc6875284d4b75d6bb5ec0c2fcb7fefc30b0d
Thoposaurus <149897944+khaleelclark@users.noreply.github.com>
2025-12-28 23:41:55 -0500

    Registered Custom time and date selctor with global context

--- ImperialFrontend/src/pages/CustomTimeAndDateSelector.tsx
+++ ImperialFrontend/src/pages/CustomTimeAndDateSelector.tsx
@@ -1,35 +1,25 @@
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
-import { Dayjs } from "dayjs";
+import dayjs, { Dayjs } from "dayjs";
 import { useCustomFormContext } from "../CustomFormContext";
 
 interface DateTimeSelectorProps {
-  value: Dayjs | null;
-  onChange: (value: Dayjs | null) => void;
   label: string;
   id: string;
 }
 
 export const CustomTimeAndDateSelector = ({
   label,
-  value,
   id,
-  onChange,
 }: DateTimeSelectorProps) => {
-  const { register } = useCustomFormContext();
-
-  const handleChange = (newValue: Dayjs | null) => {
-    // update local state in parent (e.g. FormTest)
-    onChange(newValue);
-
-    // register ISO string (UTC) for the payload -> good for DateTimeOffset
-    register(id, newValue ? newValue.toISOString() : "");
-  };
+  const { register, formValues } = useCustomFormContext();
 
   return (
     <DateTimePicker
       label={label}
-      value={value}
-      onChange={handleChange}
+      value={dayjs(formValues[id])}
+      onChange={(newValue: Dayjs | null) =>
+        register(id, newValue ? newValue.toISOString() : "")
+      }
       format="YYYY-MM-DD hh:mm A" // 12 hour format with AM/PM, can change to 24 hour by using "HH:mm"
       slotProps={{
         textField: {
