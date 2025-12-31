de61f76b8ffc59e77247742792effbc17a3625f1
khaleelclark <khaleelzindel@gmail.com>
2025-12-30 22:15:52 -0500

    Fixed db warnings

--- ImperialBackend/DTOs/GuildMemberAdminGetDTO.cs
+++ ImperialBackend/DTOs/GuildMemberAdminGetDTO.cs
@@ -2,7 +2,6 @@ namespace ImperialBackend.Models
 {
     public class GuildMemberAdminGetDTO: GuildMemberPublicGetDTO
     {
-        public int RankId { get; set; }
         public DateOnly JoinDate { get; set; }
         public int HoursPlayed { get; set; }
         public int WarsCompleted { get; set; }
--- ImperialBackend/DTOs/GuildMemberLeaderboardGetDTO.cs
+++ ImperialBackend/DTOs/GuildMemberLeaderboardGetDTO.cs
@@ -3,7 +3,7 @@ namespace ImperialBackend.Models
     public class GuildMemberLeaderboardGetDTO : GenericGetDTO
     {
 
-        public string MinecraftUsername { get; set; }
+        public string? MinecraftUsername { get; set; }
         public Guid Uuid { get; set; }
         public int HoursPlayed { get; set; }
         public int WarsCompleted { get; set; }
