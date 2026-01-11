c5b51a4f9c807bb569c37a796a953f7131386c12
khaleelclark <khaleelzindel@gmail.com>
2026-01-11 10:07:25 -0500

    Added images and OG tags for SEO

--- ImperialFrontend/rsbuild.config.ts
+++ ImperialFrontend/rsbuild.config.ts
@@ -4,13 +4,26 @@ import { pluginReact } from "@rsbuild/plugin-react";
 export default defineConfig({
   source: {
     entry: {
-      // name "index" just matches the default HTML entry
       index: "./src/index.tsx",
     },
   },
   html: {
     template: "./src/index.html",
     favicon: "./public/favicon.ico",
+    meta: {
+      description:
+        "Imperial Website. Designed and developed by esteemed members of the Imperial Guild Community.",
+      author: "pto, thop",
+      "og:title": "Imperial Website",
+      "og:description": "Imperial Website",
+      "og:image": "https://khaleelclark.com/public/imperial-og-logo.png",
+      "og:url": "https://khaleelclark.com",
+      "og:type": "website",
+      "twitter:title": "Imperial Guild",
+      "twitter:description": "Imperial Guild",
+      "twitter:card": "summary_large_image",
+      "twitter:image": "https://khaleelclark.com/public/imperial-og-logo.png",
+    },
   },
   plugins: [pluginReact()],
   tools: {
